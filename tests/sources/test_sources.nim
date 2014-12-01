ce10cd18fde00baae1bb9435b73f6681e622c4c8
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-12-01 17:47:46 +0100

    Adds highlight test of other sources. Refs #22.

--- tests/sources/test_sources.nim
+++ tests/sources/test_sources.nim
@@ -1,12 +1,15 @@
 import lazy_rest, strutils, os
 
-proc test() =
+const
+  out_dir = "output"
+
+proc render_nim_files() =
   var count = 0
   for path in walk_files("../../lazy_rest_pkg/*.nim"):
     let
-      filename = path.extract_filename.change_file_ext("html")
-      with_numbers = "numbers_" & filename
-      without_numbers = "plain_" & filename
+      name = path.split_file.name
+      with_numbers = out_dir/name & "_numbers.html"
+      without_numbers = out_dir/name & "_plain.html"
 
     with_numbers.write_file(path.source_file_to_html(number_lines = true))
     without_numbers.write_file(path.source_file_to_html(number_lines = false))
@@ -14,6 +17,29 @@ proc test() =
 
   echo "Did render ", count, " nim files."
   doAssert 6 == count, "Number of nim files didn't match, did you add some?"
+
+
+proc render_misc_files() =
+  var count = 0
+  for path in walk_files("source.*"):
+    var ext = path.split_file.ext
+    ext.delete(0, 0)
+    let
+      with_numbers = out_dir/"source_" & ext & "_numbers.html"
+      without_numbers = out_dir/"source_" & ext & "_plain.html"
+
+    with_numbers.write_file(path.source_file_to_html(number_lines = true))
+    without_numbers.write_file(path.source_file_to_html(number_lines = false))
+    count.inc
+
+  echo "Did render ", count, " misc files."
+
+
+proc test() =
+  out_dir.create_dir
+  render_nim_files()
+  render_misc_files()
+
 
 when isMainModule:
   test()
